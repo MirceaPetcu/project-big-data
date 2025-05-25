23d96652401636d1b956ff79c252171a0e002e67
MirceaPetcu <mircea.petcu@s.unibuc.ro>
2025-05-25 17:33:39 +0300

    update tune_all

--- tune_all.py
+++ tune_all.py
@@ -3,8 +3,12 @@ import subprocess
 
 
 ### tune all models with bayesian optimization ###
-models = ['en', 'lgbm', 'svr', 'rf']
+models = ['ridge', 'rf', 'en', 'lgbm', 'svr']
 objective = 'mse'
+n_trials = [200, 150, 200, 200, 100]
 
 for model in models:
-    subprocess.run(['python', 'tune.py', '--model', model, '--objective', objective])
+    subprocess.run(['python', 'tune.py',
+                    '--model', model,
+                    '--objective', objective,
+                    '--n_trials', str(n_trials[models.index(model)])])
